05f4ae4638eac287c67e5fae88339fc432be4ac7
Daniel Lee <lee@isi-solutions.org>
2015-09-11 14:28:03 +0200

    More debug

--- javascripts/script.js
+++ javascripts/script.js
@@ -34,7 +34,9 @@ $(document).ready(function(){
 
   $(window).scroll(function(){
     if(headings.length==0) return true;
+    console.log("Upper bracket");
     var scrolltop = $(window).scrollTop() || 0;
+    console.log("Lower bracket");
     console.log("Checking scroll state");
     if(headings[0] && scrolltop < headings[0].top) {
       $(".current-section").css({"opacity":0,"visibility":"hidden"});
