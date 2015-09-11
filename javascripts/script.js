8f41a3fab80e301a9aef8af7cc4fb3d812dcf64a
Daniel Lee <lee@isi-solutions.org>
2015-09-11 15:20:06 +0200

    Remove debugging outputs, false alarm

--- javascripts/script.js
+++ javascripts/script.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
     var lines = new Array(pre.length+1);
     for(var i = 0; i < pre.length; i++) {
       var wrap = Math.floor(pre[i].split("").length / 70)
-      console.log("10");
       if (pre[i]==""&&i==pre.length-1) {
         lines.splice(i, 1);
       } else {
@@ -17,7 +16,6 @@ $(document).ready(function(){
         }
       }
     }
-    console.log("20");
     $(this).before("<pre class='lines'>" + lines.join("\n") + "</pre>");
   });
 
@@ -25,31 +23,18 @@ $(document).ready(function(){
 
   var collectHeaders = function(){
     var headers = {"top":$(this).offset().top - 15,"text":$(this).text()};
-    console.log(headers);
     headings.push(headers);
-    console.log(headings);
   }
 
-  console.log("30");
-  console.log($(".markdown-body h1"));
   if($(".markdown-body h1").length > 1) $(".markdown-body h1").each(collectHeaders)
   else if($(".markdown-body h2").length > 1) $(".markdown-body h2").each(collectHeaders)
   else if($(".markdown-body h3").length > 1) $(".markdown-body h3").each(collectHeaders)
-  console.log("headings length:");
-  // This is 0, why?
-  console.log(headings.length);
 
   $(window).scroll(function(){
-    console.log("Headings length is:");
-    console.log(headings.length);
     if(headings.length==0) return true;
-    console.log("Upper bracket");
     var scrolltop = $(window).scrollTop() || 0;
-    console.log("Lower bracket");
-    console.log("Checking scroll state");
     if(headings[0] && scrolltop < headings[0].top) {
       $(".current-section").css({"opacity":0,"visibility":"hidden"});
-      console.log("40");
       return false;
     }
     $(".current-section").css({"opacity":1,"visibility":"visible"});
@@ -59,7 +44,6 @@ $(document).ready(function(){
       }
     }
   });
-  console.log("50");
 
   $(".current-section a").click(function(){
     $(window).scrollTop(0);
